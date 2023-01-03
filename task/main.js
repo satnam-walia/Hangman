186060a0da3057e59d9c5189b22a50334d8a6876
satnam <satnamwalia96@gmail.com>
2023-01-03 14:55:58 -0500

    feat(main): complete stage 4/8

--- task/main.js
+++ task/main.js
@@ -5,9 +5,15 @@ const input = require('sync-input')
 
 const wordList = Array.of("python","java","swift","javascript");
 let randomIndex = Math.floor(Math.random() * wordList.length);
-console.log("H A N G M A N",randomIndex);
+let hiddenWord = generateHyphen(wordList[randomIndex]);
+console.log("H A N G M A N");
 console.log("The game will be available soon.");
 
-let word = input("Guess the word:");
+let word = input(`Guess the word ${hiddenWord}:`);
 let status = (word === wordList[randomIndex]) ? "You survived!" : "You lost!";
 console.log(status);
+
+function generateHyphen(word){
+	let hint = word.slice(0,3);
+	return hint.padEnd(word.length,"-");
+}
