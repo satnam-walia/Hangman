54385aad5d5a3c8087aa54b5e278546fb834afd7
satnam <satnamwalia96@gmail.com>
2023-01-03 16:33:29 -0500

    feat(main): complete stage 5/8

--- task/main.js
+++ task/main.js
@@ -1,19 +1,18 @@
-// Use "input()" to input a line from the user
-// Use "input(str)" to print some text before requesting input
-// You will need this in the following stages
 const input = require('sync-input')
 
-const wordList = Array.of("python","java","swift","javascript");
-let randomIndex = Math.floor(Math.random() * wordList.length);
-let hiddenWord = generateHyphen(wordList[randomIndex]);
-console.log("H A N G M A N");
-console.log("The game will be available soon.");
+const words = ["python", "java", "swift", "javascript"]
+const nAttempts = 8
+const word = words[Math.floor(Math.random() * words.length)]
+guessed = ""
+const mask = (w) => [...w].reduce((acc, ch) => acc + (guessed.includes(ch) ? ch : '-'), "");
 
-let word = input(`Guess the word ${hiddenWord}:`);
-let status = (word === wordList[randomIndex]) ? "You survived!" : "You lost!";
-console.log(status);
-
-function generateHyphen(word){
-	let hint = word.slice(0,3);
-	return hint.padEnd(word.length,"-");
+console.log("H A N G M A N")
+for (let i = 0; i < nAttempts; i++) {
+	const ch = input(`\n${mask(word)}\nInput a letter: `)
+	if (word.includes(ch)) {
+		guessed += ch
+	} else {
+		console.log("That letter doesn't appear in the word.")
+	}
 }
+console.log("Thanks for playing!")
