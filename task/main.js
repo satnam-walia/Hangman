4a257a0134c9d68446257bac7671cb2d6dd45149
satnam <satnamwalia96@gmail.com>
2023-01-03 18:29:38 -0500

    feat(main):complete stage 7/8

--- task/main.js
+++ task/main.js
@@ -3,30 +3,40 @@ const input = require('sync-input')
 const words = ["python", "java", "swift", "javascript"]
 const word = words[Math.floor(Math.random() * words.length)]
 let nAttempts = 8
-let guessed = ""
-const mask = (w) => [...w].reduce((acc, ch) => acc + (guessed.includes(ch) ? ch : '-'), "");
+let goodGuess = ""
+let badGuess = ""
+const mask = (w) => [...w].reduce((acc, ch) => acc + (goodGuess.includes(ch) ? ch : '-'), "");
 
 console.log("H A N G M A N")
 while(nAttempts!==0) {
 	const ch = input(`\n${mask(word)}\nInput a letter: `)
-	if (word.includes(ch)) {
-		if(guessed.includes(ch)) {
-			nAttempts--;
-			console.log("No improvements.");
-		}
-		guessed += ch
-	} else {
-		nAttempts--;
-		console.log("That letter doesn't appear in the word.")
-	}
+	if(ch.length == 1) {
+		if(ch.match(/[a-z]/g)) {
 
-	if(!mask(word).includes("-")) {
-		nAttempts = 0;
-		console.log("You guessed the word!");
-		console.log("You survived!");
-		break;
-	}else if(mask(word).includes("-") && nAttempts == 0) {
-		console.log("You lost!");
+			if (badGuess.includes(ch) || goodGuess.includes(ch)) {
+				console.log("You've already guessed this letter.");
+				continue;
+			}
+			if (word.includes(ch)) {
+				goodGuess += ch
+			} else {
+				nAttempts--;
+				badGuess += ch
+				console.log("That letter doesn't appear in the word.")
+			}
+			if (!mask(word).includes("-")) {
+				nAttempts = 0;
+				console.log(`You guessed the word ${mask(word)}!`);
+				console.log("You survived!");
+				break;
+			} else if (mask(word).includes("-") && nAttempts == 0) {
+				console.log("You lost!");
+			}
+		}else{
+			console.log("Please, enter a lowercase letter from the English alphabet.");
+		}
+	}else{
+		console.log("Please, input a single letter.")
 	}
 }
 
